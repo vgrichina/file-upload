a36b4498680b22c65c2245b3d8c4dba243cf7acd
Vladimir Grichina <vgrichina@gmail.com>
2010-01-22 02:11:23 +0200

    Added code to actually write upload to file

--- upload.js
+++ upload.js
@@ -2,6 +2,7 @@ var http = require('http');
 var url = require('url');
 var multipart = require('multipart');
 var sys = require('sys');
+var posix = require("posix");
 
 var server = http.createServer(function(req, res) {
   switch (url.parse(req.url).pathname) {
@@ -34,16 +35,42 @@ function upload_file(req, res) {
 
   var stream = new multipart.Stream(req);
   stream.addListener('part', function(part) {
+    // Ask to open/create file
+    var fileOpen = posix.open("./tmp.file", process.O_CREAT | process.O_WRONLY, 0600);
+    var fileDescriptor = null;
+    fileOpen.addCallback(function(fd) {
+        fileDescriptor = fd;
+    });
+
     part.addListener('body', function(chunk) {
       var progress = (stream.bytesReceived / stream.bytesTotal * 100).toFixed(2);
       var mb = (stream.bytesTotal / 1024 / 1024).toFixed(1);
-
-      sys.print("Uploading "+mb+"mb ("+progress+"%)\015");
+ 
+      sys.debug("Uploading "+mb+"mb ("+progress+"%)");
 
       // chunk could be appended to a file if the uploaded file needs to be saved
+      
+      // Wait for file to be opened if needed
+      if (fileDescriptor == null) {
+          sys.debug("Waiting for file");
+          fileOpen.wait();
+      }
+
+      // Write chunk to file
+      sys.debug("Writing chunk");
+      posix.write(fileDescriptor, chunk).wait();
+   
+      // Close file if needed
+      if (stream.bytesReceived == stream.bytesTotal) {
+          sys.debug("Closing file");
+          posix.close(fileDescriptor).wait();
+          sys.debug("Closed file");
+      }
     });
   });
   stream.addListener('complete', function() {
+    sys.debug("Request complete");
+
     res.sendHeader(200, {'Content-Type': 'text/plain'});
     res.sendBody('Thanks for playing!');
     res.finish();
