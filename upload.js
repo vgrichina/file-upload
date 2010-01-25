a9a1bccb88f863f072d79ce1eecd0e44c01b041f
Vladimir Grichina <vgrichina@gmail.com>
2010-01-25 03:11:04 +0200

    Fixed event handling so that response is only given when file is closed, also added throttling for request data so that no chunks are lost

--- upload.js
+++ upload.js
@@ -2,6 +2,7 @@ var http = require("http");
 var url = require("url");
 var multipart = require("multipart");
 var sys = require("sys");
+var events = require("events");
 var posix = require("posix");
 
 var server = http.createServer(function(req, res) {
@@ -22,7 +23,6 @@ var server = http.createServer(function(req, res) {
 // Server would listen on port 800
 server.listen(8000);
 
-
 /*
  * Display upload form
  */
@@ -38,64 +38,70 @@ function display_form(req, res) {
 }
 
 /*
- * Handle file upload
+ * Write chunk of uploaded file
  */
-function write_chunk(fileDescriptor, chunk, isLast) {
+function write_chunk(request, fileDescriptor, chunk, isLast, closePromise) {
+    // Pause receiving request data (until current chunk is written)
+    request.pause();
     // Write chunk to file
     sys.debug("Writing chunk");
     posix.write(fileDescriptor, chunk).addCallback(function() {
+        sys.debug("Wrote chunk");
+        // Resume receiving request data
+        request.resume();
         // Close file if completed
         if (isLast) {
             sys.debug("Closing file");
             posix.close(fileDescriptor).addCallback(function() {
                 sys.debug("Closed file");
+                
+                // Emit file close promise
+                closePromise.emitSuccess();
             });
         }
     });
 }
 
+/*
+ * Handle file upload
+ */
 function upload_file(req, res) {
+    // Request body is binary
     req.setBodyEncoding("binary");
 
     // Handle request as multipart
     var stream = new multipart.Stream(req);
     
+    // Create promise that will be used to emit event on file close
+    var closePromise = new events.Promise();
+
     // Add handler for a request part received
     stream.addListener("part", function(part) {
         sys.debug("Received part, name = " + part.name + ", filename = " + part.filename);
         
-        var fileDescriptor = null;
         var openPromise = null;
 
         // Add handler for a request part body chunk received
         part.addListener("body", function(chunk) {
+            // Calculate upload progress
             var progress = (stream.bytesReceived / stream.bytesTotal * 100).toFixed(2);
             var mb = (stream.bytesTotal / 1024 / 1024).toFixed(1);
      
             sys.debug("Uploading " + mb + "mb (" + progress + "%)");
 
-            // Wait for file to be opened if needed
-            if (fileDescriptor == null) {
-                sys.debug("Waiting for file");
-                // Ask to open/create file (if not asked before)
-                if (openPromise == null) {
-                    sys.debug("Opening file");
-                    openPromise = posix.open("./uploads/" + part.filename, process.O_CREAT | process.O_WRONLY, 0600);
-                }
-                openPromise.addCallback(function(fd) {
-                    sys.debug("File opened");
-                    // Write chunk to file
-                    fileDescriptor = fd;
-                    write_chunk(fileDescriptor, chunk, (stream.bytesReceived == stream.bytesTotal));
-                });
-            } else {
-                // Write chunk to file
-                write_chunk(fileDescriptor, chunk, (stream.bytesReceived == stream.bytesTotal));
+            // Ask to open/create file (if not asked before)
+            if (openPromise == null) {
+                sys.debug("Opening file");
+                openPromise = posix.open("./uploads/" + part.filename, process.O_CREAT | process.O_WRONLY, 0600);
             }
-        });
 
-        part.addListener("complete", function() {
-            sys.debug("Part complete"); 
+            // Add callback to execute after file is opened
+            // If file is already open it is executed immediately
+            openPromise.addCallback(function(fileDescriptor) {
+                // Write chunk to file
+                write_chunk(req, fileDescriptor, chunk, 
+                    (stream.bytesReceived == stream.bytesTotal), closePromise);
+            });
         });
     });
 
@@ -103,12 +109,15 @@ function upload_file(req, res) {
     stream.addListener("complete", function() {
         sys.debug("Request complete");
 
-        // TODO: Wait until all chunks are written
-        res.sendHeader(200, {"Content-Type": "text/plain"});
-        res.sendBody("Thanks for playing!");
-        res.finish();
-
-        sys.puts("\n=> Done");
+        // Wait until file is closed
+        closePromise.addCallback(function() {
+            // Render response
+            res.sendHeader(200, {"Content-Type": "text/plain"});
+            res.sendBody("Thanks for playing!");
+            res.finish();
+        
+            sys.puts("\n=> Done");
+        });
     });
 }
 
